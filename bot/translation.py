0cb03721ffd4e6c5906488e9b50f6156ae26cee9
LuciferMornigstarDevil <91133525+LuciferMornigstarDevil@users.noreply.github.com>
2021-09-23 12:42:28 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,63 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG @MRK_YT
+# (c) @AlbertEinsteinTG
 
 class Translation(object):
     
-    START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
+    START_TEXT = """<b>Hey {}!!
 
-<b>I am a Pro AutoFilterBot V2....😜</b>
+ഞാൻ 🛡️<a href="https://t.me/ADMOVEIAD">𝙰𝙳 𝙼𝙾𝚅𝙸𝙴𝚂 𝙷𝚄𝙽𝚃</a>🛡️ എന്ന ഗ്രൂപ്പിൽ മൂവീസ് കൊടുക്കുന്ന ബോട്ട് ആണ് 
+ 
+നോക്കണ്ടാ എന്നെ മറ്റു ഗ്രൂപ്പിൽ ഒന്നും ഉപയോഗിക്കാൻ കഴിയുകയില്ല
 
-<b>Make me an admin for your group and channel then connect me....🎉</b>
-
-<b>🙂Watch the video below to build an Pro Autofilter bot V2 like me...</b>
-
-<b>Subscribe to the update channel to learn about my updates and activity...</b>
-
-<b>Press /help to know about available commands🤪</b>"""    
+©️MᴀɪɴᴛᴀɪɴᴇD Bʏ : <a href="https://t.me/Lucifer_Devil_AD">꧁𝗟𝘂ᴥ𝗰𝗶𝗳𝗲𝗿ʔ❍𝗠𝗼ᗕ𝗿𝗻ಠ𝗶𝗴𝘀𝘁ಠ𝗮𝗿꧂</a>
+</b>"""    
     
     HELP_TEXT = """
-<b>😉How To Use Me!😉?</b>
-
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - <b>To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)</b>
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - <b>To disconnect A Group With A Channel</b>
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - <b>This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB</b>
-    
-    -> <code>/settings</code> -  <b>This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly</b>
-
-            -> <code>Channel</code> - <b>Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls</b>
-            
-            -> <code>Filter Types</code> - <b>Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart</b>
-
-            -> <code>Configure</code> - <b>Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results</b>
-            
-            -> <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
-            
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
+<b>നോക്കണ്ടെടാ ഉണ്ണി ഇത് നീ ഉദ്ദേശിച്ചത് അല്ല 😹 !.</b>
 """
     
-    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
+    ABOUT_TEXT = """<b>❍ ɴᴀᴍᴇ</b> : <code> JD</code>
+    
+<b>❍ ᴄʀᴇᴀᴛᴏʀ</b> : <b><i><a href="https://t.me/Lucifer_Devil_AD">꧁𝗟𝘂ᴥ𝗰𝗶𝗳𝗲𝗿ʔ❍𝗠𝗼ᗕ𝗿𝗻ಠ𝗶𝗴𝘀𝘁ಠ𝗮𝗿꧂</a></i></b>
 
-<b>>➥👨‍💼Master</b> : <b><a href="https://t.me/AlbertEinstein_TG">👤Albert Einstein</a></b>
+<b>❍ ʟᴀɴɢᴜᴀɢᴇ</b> : <code>Python3</code>
 
-<b>➥ 👨‍💻Editor</b> : <b><a href="https://t.me/MRk_YT">👤Mrk YT</a></b>
+<b>❍ ʟɪʙʀᴀʀʏ</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ 🗣️Language</b> : <b>Python3<b>
-
-<b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
-
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MoTechYT/Pro-Auto-Filter-Bot-V2">💥Click Me</a></b>
+<b>❍ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ</b> : 🔐
 """
